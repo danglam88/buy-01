c492751907ab1ea6ddedd4114b3732a1bcf3eff6
Dang Lam <dang.lam@gritlab.ax>
2023-10-20 20:41:43 +0300

    certificates work from frontend to backend (via localhost) but not among microservices (via service-names)

---
+++ backend/media/src/main/java/com/gritlab/config/RestTemplateConfig.java
@@ -0,0 +1,49 @@
+package com.gritlab.config;
+
+import org.apache.hc.client5.http.impl.async.CloseableHttpAsyncClient;
+import org.apache.hc.client5.http.impl.async.HttpAsyncClients;
+import org.apache.hc.client5.http.impl.nio.PoolingAsyncClientConnectionManagerBuilder;
+import org.apache.hc.client5.http.nio.AsyncClientConnectionManager;
+import org.apache.hc.client5.http.ssl.DefaultClientTlsStrategy;
+import org.apache.hc.core5.http.nio.ssl.TlsStrategy;
+import org.apache.hc.core5.ssl.SSLContexts;
+import org.apache.hc.core5.ssl.TrustStrategy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+import javax.net.ssl.SSLContext;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
+
+@Configuration
+public class RestTemplateConfig {
+
+    @Bean
+    public RestTemplate restTemplate(){
+        try {
+            TrustStrategy acceptingTrustStrategy = (X509Certificate[] chain, String authType) -> true;
+
+            SSLContext sslContext = SSLContexts.custom()
+                    .loadTrustMaterial(null, acceptingTrustStrategy)
+                    .build();
+
+            TlsStrategy tlsStrategy = new DefaultClientTlsStrategy(sslContext);
+
+            AsyncClientConnectionManager connManager = PoolingAsyncClientConnectionManagerBuilder.create()
+                    .setTlsStrategy(tlsStrategy)
+                    .build();
+
+            CloseableHttpAsyncClient httpClient = HttpAsyncClients.custom()
+                    .setConnectionManager(connManager)
+                    .build();
+
+            return new RestTemplate();
+        } catch (NoSuchAlgorithmException | KeyManagementException | KeyStoreException e) {
+            e.printStackTrace();
+            return new RestTemplate();
+        }
+    }
+}
