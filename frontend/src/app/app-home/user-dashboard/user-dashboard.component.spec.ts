484f4276f3bf5697a81593d19cb66994090c2718
hgwtra <huong.mai198@gmail.com>
2023-11-02 17:28:12 +0200

    test cases for user dashboard

--- frontend/src/app/app-home/user-dashboard/user-dashboard.component.spec.ts
+++ frontend/src/app/app-home/user-dashboard/user-dashboard.component.spec.ts
@@ -7,12 +7,14 @@ import { HeaderComponent } from '../header/header.component';
 import { AngularMaterialModule } from 'src/app/angular-material.module';
 import { FooterComponent } from '../footer/footer.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { of } from 'rxjs';
 
 describe('UserDashboardComponent', () => {
   let component: UserDashboardComponent;
   let fixture: ComponentFixture<UserDashboardComponent>;
 
   beforeEach(() => {
+    let userService: UserService;
     TestBed.configureTestingModule({
       declarations: [UserDashboardComponent, 
                     HeaderComponent,
@@ -25,10 +27,39 @@ describe('UserDashboardComponent', () => {
     });
     fixture = TestBed.createComponent(UserDashboardComponent);
     component = fixture.componentInstance;
+    userService = TestBed.inject(UserService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should update user name', () => {
+    const updatedName = 'Ashley'; 
+    component.userProfileForm.patchValue({ name: updatedName });
+    component.updateUserInformation('name');
+    expect(component.userProfileForm.value.name).toEqual(updatedName);
+  });
+  
+  it('should update user email', () => {
+    const updatedEmail = 'Ashley@gmail.com'; 
+    component.userProfileForm.patchValue({ email: updatedEmail });
+    component.updateUserInformation('email');
+    expect(component.userProfileForm.value.email).toEqual(updatedEmail);
+  });
+
+  it('should update user password', () => {
+    const updatedPassword = 'Ashley1234!'; 
+    component.userProfileForm.patchValue({ password: updatedPassword });
+    component.updateUserInformation('password');
+    expect(component.userProfileForm.value.password).toEqual(updatedPassword);
+  });
+
+  it('should update user avatar', () => {
+    const updatedAvatar = undefined; 
+    component.userProfileForm.patchValue({ avatar: updatedAvatar });
+    component.updateUserInformation('avatar');
+    expect(component.userProfileForm.value.avatar).toEqual(updatedAvatar); 
   });
 });
