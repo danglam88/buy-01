35a09fe424f30ffee57a9b1e49d6a22676e1d5c2
danglam88 <dang.lam.4688@gmail.com>
2023-11-24 20:38:49 +0200

    some fixes

--- backend/setup.sh
+++ backend/setup.sh
@@ -7,13 +7,10 @@ if ! docker volume ls -q | grep -q '^mongo-volume$'; then
     docker volume create mongo-volume
 fi
 
-cp user/forfrontend/Dockerfile user/
-docker build -t user-microservice -f user/Dockerfile .
+docker build -t user-microservice -f user/forfrontend/Dockerfile .
 
-cp product/forfrontend/Dockerfile product/
-docker build -t product-microservice -f product/Dockerfile .
+docker build -t product-microservice -f product/forfrontend/Dockerfile .
 
-cp media/forfrontend/Dockerfile media/
-docker build -t media-microservice -f media/Dockerfile .
+docker build -t media-microservice -f media/forfrontend/Dockerfile .
 
 docker-compose up -d
