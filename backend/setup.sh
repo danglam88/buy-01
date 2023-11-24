95c7b1d7be2a5551c3008a618fd0eac71cd65362
danglam88 <dang.lam.4688@gmail.com>
2023-11-24 19:22:01 +0200

    fix mongodb port and volume

--- backend/setup.sh
+++ backend/setup.sh
@@ -1,16 +1,11 @@
 #!/bin/bash -e
 
 docker-compose down
-docker system prune -a -f
+docker system prune -a -f --volumes
 
 if ! docker volume ls -q | grep -q '^mongo-volume$'; then
     docker volume create mongo-volume
 fi
-
-echo "Current directory:"
-pwd
-echo "Directory contents:"
-ls -la
 
 cp user/forfrontend/Dockerfile user/
 docker build -t user-microservice -f user/Dockerfile .
