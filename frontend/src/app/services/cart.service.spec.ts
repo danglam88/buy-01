ec00b6f4f3dd88ecdc3f0bacd088ad9e958dff8c
Trà <huong.mai198@gmail.com>
2023-12-01 15:43:25 +0200

    unit test 1/3

--- frontend/src/app/services/cart.service.spec.ts
+++ frontend/src/app/services/cart.service.spec.ts
@@ -1,16 +1,84 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { CartService } from './cart.service';
+import { Product } from '../Models/Product';
 
 describe('CartService', () => {
-  let service: CartService;
+  let cartService: CartService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(CartService);
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [CartService]
+    });
+
+    cartService = TestBed.inject(CartService);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(cartService).toBeTruthy();
   });
+
+  it('should add product to cart', () => {
+    const product: Product = {
+      id: '1',
+      name: 'Test Product',
+      description: 'Product for testing',
+      price: 10,
+      quantity: 5,
+      editable: true
+    };
+
+    cartService.addToCart(product);
+
+    const cart = cartService.getCart();
+    expect(cart.items.length).toBe(1);
+    expect(cart.items[0].product).toEqual(product);
+  });
+
+  it('should remove product from cart', () => {
+    const productId = '1';
+    const product: Product = {
+      id: productId,
+      name: 'Test Product',
+      description: 'Product for testing',
+      price: 10,
+      quantity: 5,
+      editable: true
+    };
+
+    cartService.addToCart(product);
+
+    let cart = cartService.getCart();
+    expect(cart.items.length).toBe(1);
+
+    cartService.removeFromCart(productId);
+
+    cart = cartService.getCart();
+    expect(cart.items.length).toBe(0);
+  });
+
+  it('should change quantity of product in cart', () => {
+    const productId = '1';
+    const product: Product = {
+      id: productId,
+      name: 'Test Product',
+      description: 'Product for testing',
+      price: 10,
+      quantity: 5,
+      editable: true
+    };
+
+    cartService.addToCart(product);
+
+    let cart = cartService.getCart();
+    expect(cart.items.length).toBe(1);
+
+    const newQuantity = 3;
+    cartService.changeQuantity(productId, newQuantity);
+
+    cart = cartService.getCart();
+    expect(cart.items[0].quantity).toBe(newQuantity);
+  });
+
 });
