46efe3b7e0a12b6f6233e08b75a00ee8784ecf05
Iuliia Chipsanova <iuliia.chipsanova@gritlab.ax>
2023-12-09 18:54:36 +0200

    fixed code quality issues from sonarqube report

--- backend/order/src/main/java/com/gritlab/repository/OrderItemRepositoryCustom.java
+++ backend/order/src/main/java/com/gritlab/repository/OrderItemRepositoryCustom.java
@@ -19,67 +19,73 @@ public class OrderItemRepositoryCustom {
 
     private final MongoTemplate mongoTemplate;
 
+    private static final String PRODUCT_ID = "productId";
+    private static final String ORDER_ITEM = "order_item";
+    private static final String TOTAL_QUANTITY = "totalQuantity";
+    private static final String STATUS_CODE = "statusCode";
+    private static final String TOTAL_ITEM_PRICE = "totalItemPrice";
+
     public OrderItemRepositoryCustom(MongoTemplate mongoTemplate) {
         this.mongoTemplate = mongoTemplate;
     }
 
     public List<TopProduct> sumQuantityByProductIdAndBuyerId(String buyerId) {
-        GroupOperation groupOperation = Aggregation.group("productId")
-                .first("productId").as("productId")
+        GroupOperation groupOperation = Aggregation.group(PRODUCT_ID)
+                .first(PRODUCT_ID).as(PRODUCT_ID)
                 .first("name").as("name")
-                .sum("quantity").as("totalQuantity");
+                .sum("quantity").as(TOTAL_QUANTITY);
 
         Aggregation aggregation = Aggregation.newAggregation(
-                Aggregation.match(Criteria.where("buyerId").is(buyerId).and("statusCode").is(OrderStatus.CONFIRMED)),
+                Aggregation.match(Criteria.where("buyerId").is(buyerId).and(STATUS_CODE).is(OrderStatus.CONFIRMED)),
                 groupOperation,
-                Aggregation.sort(Sort.Direction.DESC, "totalQuantity"),
+                Aggregation.sort(Sort.Direction.DESC, TOTAL_QUANTITY),
                 Aggregation.limit(1)
         );
 
-        AggregationResults<TopProduct> results = mongoTemplate.aggregate(aggregation, "order_item", TopProduct.class);
+        AggregationResults<TopProduct> results = mongoTemplate.aggregate(aggregation, ORDER_ITEM, TopProduct.class);
         return  results.getMappedResults();
     }
 
     public List<TopProduct> sumQuantityByProductIdAndSellerId(String sellerId) {
-        GroupOperation groupOperation = Aggregation.group("productId")
-                .first("productId").as("productId")
+        GroupOperation groupOperation = Aggregation.group(PRODUCT_ID)
+                .first(PRODUCT_ID).as(PRODUCT_ID)
                 .first("name").as("name")
-                .sum("quantity").as("totalQuantity");
+                .sum("quantity").as(TOTAL_QUANTITY);
 
         Aggregation aggregation = Aggregation.newAggregation(
-                Aggregation.match(Criteria.where("sellerId").is(sellerId).and("statusCode").is(OrderStatus.CONFIRMED)),
+                Aggregation.match(Criteria.where("sellerId").is(sellerId).and(STATUS_CODE).is(OrderStatus.CONFIRMED)),
                 groupOperation,
-                Aggregation.sort(Sort.Direction.DESC, "totalQuantity"),
+                Aggregation.sort(Sort.Direction.DESC, TOTAL_QUANTITY),
                 Aggregation.limit(1)
         );
 
-        AggregationResults<TopProduct> results = mongoTemplate.aggregate(aggregation, "order_item", TopProduct.class);
+        AggregationResults<TopProduct> results = mongoTemplate.aggregate(aggregation, ORDER_ITEM, TopProduct.class);
         return results.getMappedResults();
     }
 
     public Double getTotalSumItemPriceByBuyerId(String buyerId) {
         Aggregation aggregation = Aggregation.newAggregation(
-                Aggregation.match(Criteria.where("buyerId").is(buyerId).and("statusCode").is(OrderStatus.CONFIRMED)),
+                Aggregation.match(Criteria.where("buyerId").is(buyerId).and(STATUS_CODE).is(OrderStatus.CONFIRMED)),
                 Aggregation.group()
-                        .sum("itemPrice").as("totalItemPrice")
+                        .sum("itemPrice").as(TOTAL_ITEM_PRICE)
         );
 
-        AggregationResults<Document> results = mongoTemplate.aggregate(aggregation, "order_item", Document.class);
+        AggregationResults<Document> results = mongoTemplate.aggregate(aggregation, ORDER_ITEM, Document.class);
         Document resultDocument = results.getUniqueMappedResult();
 
-        return resultDocument == null ? 0.0 : resultDocument.getDouble("totalItemPrice");
+        return resultDocument == null ? 0.0 : resultDocument.getDouble(TOTAL_ITEM_PRICE);
     }
 
     public Double getTotalSumItemPriceBySellerId(String sellerId) {
         Aggregation aggregation = Aggregation.newAggregation(
-                Aggregation.match(Criteria.where("sellerId").is(sellerId).and("statusCode").is(OrderStatus.CONFIRMED)),
+                Aggregation.match(Criteria.where("sellerId").is(sellerId).and(STATUS_CODE).is(OrderStatus.CONFIRMED)),
                 Aggregation.group()
-                        .sum("itemPrice").as("totalItemPrice")
+                        .sum("itemPrice").as(TOTAL_ITEM_PRICE)
         );
 
-        AggregationResults<Document> results = mongoTemplate.aggregate(aggregation, "order_item", Document.class);
+        AggregationResults<Document> results = mongoTemplate.aggregate(aggregation, ORDER_ITEM, Document.class);
         Document resultDocument = results.getUniqueMappedResult();
 
-        return resultDocument == null ? 0.0 : resultDocument.getDouble("totalItemPrice");
+        return resultDocument == null ? 0.0 : resultDocument.getDouble(TOTAL_ITEM_PRICE);
     }
 }
