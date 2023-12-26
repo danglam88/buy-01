ffc213b6f83e8345f9b33f7bafcf7d32072645b4
Trà <huong.mai198@gmail.com>
2023-12-26 14:41:47 +0200

    fixed session expired when a user is deleted

--- frontend/src/app/services/error.service.ts
+++ frontend/src/app/services/error.service.ts
@@ -1,26 +1,35 @@
-import { Injectable } from '@angular/core';
-import { ToastrService } from 'ngx-toastr';
-import { Router } from '@angular/router';
+import { Injectable } from "@angular/core";
+import { ToastrService } from "ngx-toastr";
+import { Router } from "@angular/router";
+import { EncryptionService } from "./encryption.service";
+import { AuthenticationService } from "./authentication.service";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ErrorService {
-
-  constructor(private toastr: ToastrService, private router: Router) { }
+  constructor(
+    private toastr: ToastrService,
+    private router: Router,
+    private encryptionService: EncryptionService,
+    private authService: AuthenticationService
+  ) {}
 
   handleSessionExpirationError(): void {
-    this.toastr.error('Session expired. Log-in again.');
-    this.router.navigate(['../login']);
+    this.toastr.error("Session expired. Log-in again.");
+    this.authService.logout(
+      
+    );
+
   }
 
   handleBadRequestError(error: any): void {
-      if (error.error.message) {
-        this.toastr.error(error.error.message);
-      } else if (error.error && error.error[0]) {
-        this.toastr.error(error.error[0]);
-      } else {
-        this.toastr.error('Something went wrong');
+    if (error.error.message) {
+      this.toastr.error(error.error.message);
+    } else if (error.error && error.error[0]) {
+      this.toastr.error(error.error[0]);
+    } else {
+      this.toastr.error("Something went wrong");
     }
   }
 
