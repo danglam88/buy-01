e7277018ea68a91eefb8688278548ffc915023f9
Trà <huong.mai198@gmail.com>
2023-12-26 16:07:49 +0200

    removed duplicated values for search

--- frontend/src/app/app-home/search/search.component.ts
+++ frontend/src/app/app-home/search/search.component.ts
@@ -1,27 +1,39 @@
-import { Component, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Output,
+  ViewChild,
+  ElementRef,
+} from "@angular/core";
 
 @Component({
-  selector: 'app-search',
-  templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  selector: "app-search",
+  templateUrl: "./search.component.html",
+  styleUrls: ["./search.component.css"],
 })
 export class SearchComponent {
-  
   searchText: string[] = [];
 
-@Output()
- searchTextChanged: EventEmitter<string[]> = new EventEmitter<string[]>()
+  @Output()
+  searchTextChanged: EventEmitter<string[]> = new EventEmitter<string[]>();
 
- @ViewChild('searchInput') searchInputEl: ElementRef;
+  @ViewChild("searchInput") searchInputEl: ElementRef;
 
   updateSearchText() {
-    this.searchText = this.searchInputEl.nativeElement.value.split(' ').filter((word: string) => word.trim() != '');
+    const inputValue = this.searchInputEl.nativeElement.value;
+
+    // Split the input value into an array of words
+    const words = inputValue
+      .split(" ")
+      .filter((word: string) => word.trim() !== "");
+
+    // Use a Set to store unique words and convert it back to an array
+    this.searchText = Array.from(new Set(words));
+
     this.searchTextChanged.emit(this.searchText);
   }
-
   cancelSearch(item: string) {
     this.searchText = this.searchText.filter((word: string) => word != item);
     this.searchTextChanged.emit(this.searchText);
   }
-
 }
