691bc7778200639014788680f7806b4123fa425a
Trà <huong.mai198@gmail.com>
2024-01-03 03:18:13 +0200

    unit tests

--- frontend/src/app/app-home/my-orders/order-history/order-history.component.spec.ts
+++ frontend/src/app/app-home/my-orders/order-history/order-history.component.spec.ts
@@ -1,48 +1,64 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { AngularMaterialModule } from 'src/app/angular-material.module';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { ToastrService } from 'ngx-toastr';
-
+import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 import { OrderHistoryComponent } from './order-history.component';
-import { OrderDetailsComponent } from '../order-details/order-details.component';
-import { ConfirmationDialogComponent } from '../../confirmation-dialog/confirmation-dialog.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { ToastrModule } from 'ngx-toastr';
+import { OrderService } from 'src/app/services/order.service';
+import { of } from 'rxjs';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { AngularMaterialModule } from 'src/app/angular-material.module';
 import { SearchComponent } from '../../search/search.component';
 
-import { UserService } from 'src/app/services/user.service';
-import { OrderService } from 'src/app/services/order.service';
-import { OrderItemService } from 'src/app/services/order-item.service';
-
-class ToastrServiceStub {
-  error(message: string) {}
-  success(message: string) {}
-}
 
 describe('OrderHistoryComponent', () => {
   let component: OrderHistoryComponent;
   let fixture: ComponentFixture<OrderHistoryComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        OrderHistoryComponent, 
-        OrderDetailsComponent, 
-        ConfirmationDialogComponent,
-        SearchComponent
-      ],
+  let orderService: jasmine.SpyObj<OrderService>;
+
+  beforeEach(async () => {
+    orderService = jasmine.createSpyObj('OrderService', ['getClientOrders', 'redoOrder', 'getSellerOrderItems', 'cancelOrder', 'removeOrder']);
+
+    await TestBed.configureTestingModule({
+      declarations: [OrderHistoryComponent, SearchComponent],
+      imports: [MatDialogModule, ToastrModule.forRoot(), HttpClientTestingModule, AngularMaterialModule],
       providers: [
-        UserService, 
-        OrderService, 
-        OrderItemService,
-        { provide: ToastrService, useClass: ToastrServiceStub },
+        { provide: OrderService, useValue: { 
+          getClientOrders: () => of({ orders: [] }),
+          redoOrder: () => of({}),
+        } },
       ],
-      imports: [AngularMaterialModule, HttpClientTestingModule]
-    });
+    }).compileComponents();
+  });
+  
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(OrderHistoryComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should get client orders with seller info for CLIENT role', fakeAsync(() => {
+    component.role = 'CLIENT';
+    spyOn(component, 'getClientOrdersWithSellerInfo').and.callThrough();
+    
+    component.ngOnInit();
+    tick();
+
+    expect(component.getClientOrdersWithSellerInfo).toHaveBeenCalled();
+  }));
+
+  it('should get seller order items for SELLER role', fakeAsync(() => {
+    component.role = 'SELLER';
+    spyOn(component, 'getSellerOrderItems').and.callThrough();
+    
+    component.ngOnInit();
+    tick();
+
+    expect(component.getSellerOrderItems).toHaveBeenCalled();
+  }));
+
 });
